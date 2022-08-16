19281106ccaf51d1991d736b710acef733951b04
ByeongHyeon <qudgus1984@naver.com>
2022-08-16 18:51:58 +0900

    [SeSAC] Week7 Day 1 PageViewController / Framework / animation

--- SeSAC2Week7Diary/ViewController.swift
+++ SeSAC2Week7Diary/ViewController.swift
@@ -24,13 +24,13 @@ class ViewController: UIViewController {
         
         testOpen()
         
-        showSesacAlert(title: "테스트 얼럿", message: "테스트 메세지", buttonTitle: "변경") { _ in
-            self.view.backgroundColor = .lightGray
-        }
-//        let image = UIImage(systemName: "star.fill")!
-//        let shareURL = "https://www.apple.com"
-//        let text = "WWDC What's New!!!"
-//        sesacShowActivityViewController(shareImage: image, shareURL: shareURL, shareText: text)
+//        showSesacAlert(title: "테스트 얼럿", message: "테스트 메세지", buttonTitle: "변경") { _ in
+//            self.view.backgroundColor = .lightGray
+//        }
+        let image = UIImage(systemName: "star.fill")!
+        let shareURL = "https://www.apple.com"
+        let text = "WWDC What's New!!!"
+        sesacShowActivityViewController(shareImage: image, shareURL: shareURL, shareText: text)
         
         OpenWebView.presentWebViewController(self, url: "https://www.naver.com", transitionStyle: .present)
         
